2c6dfdd8758ddfe9970f97dbb535780ee49c0182
liguanlin <517445163@qq.com>
2023-12-18 22:34:34 +0800

    update detection demo

--- detection_demo.py
+++ detection_demo.py
@@ -8,8 +8,12 @@ import cv2
 import shutil
 
 # 指定模型的配置文件和 checkpoint 文件路径
-config_file = '/data/liguanlin/codes/mmdetection/yolov3_mobilenetv2_320_300e_coco.py'
-checkpoint_file = '/data/liguanlin/codes/mmdetection/yolov3_mobilenetv2_320_300e_coco_20210719_215349-d18dff72.pth'
+config_file = '/data/liguanlin/codes/mmdetection/configs/yolox/yolox_x_8x8_300e_coco.py'
+checkpoint_file = '/data/liguanlin/codes/mmdetection/yolox_x_8x8_300e_coco_20211126_140254-1ef88d67.pth'
+#config_file = '/data/liguanlin/codes/mmdetection/configs/dino/dino-5scale_swin-l_8xb2-36e_coco.py'
+#checkpoint_file = '/data/liguanlin/codes/mmdetection/yolov3_mobilenetv2_320_300e_coco_20210719_215349-d18dff72.pth'
+#config_file = '/data/liguanlin/codes/codes_from_github/mmdetection/configs/deformable_detr/deformable-detr-refine-twostage_r50_16xb2-50e_coco.py'
+#checkpoint_file = '/data/liguanlin/codes/mmdetection/deformable-detr-refine-twostage_r50_16xb2-50e_coco_20221021_184714-acc8a5ff.pth'
 
 # 根据配置文件和 checkpoint 文件构建模型
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
@@ -29,7 +33,7 @@ if not os.path.exists(temp_out_dir):
     os.mkdir(temp_out_dir)
 
 # 测试视频并展示结果
-video = mmcv.VideoReader('input/DJI_0024.MP4')
+video = mmcv.VideoReader('input/DJI_0286_enhanced.mp4')
 # 读入待预测视频
 #imgs = mmcv.VideoReader(input_video)
 
@@ -53,7 +57,7 @@ for frame in video:
     model.show_result(frame, result, wait_time=1,out_file=)
 """
 # 把每一帧串成视频文件
-mmcv.frames2video(temp_out_dir, 'output/DJI_0024_detected.mp4', fps=video.fps, fourcc='mp4v')
+mmcv.frames2video(temp_out_dir, 'output/DJI_0286_enhanced_detected_yolox.mp4', fps=video.fps, fourcc='mp4v')
 
 shutil.rmtree(temp_out_dir) # 删除存放每帧画面的临时文件夹
 print('删除临时文件夹', temp_out_dir)
